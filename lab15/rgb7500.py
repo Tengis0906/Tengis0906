d8d30ee50a0901c76fb7c816efa4b9765bb66dfb
Tengis <is23d010@mandakh.edu.mn>
2023-12-08 15:53:37 +0800

    123

---
+++ lab15/rgb7500.py
@@ -0,0 +1,10 @@
+# import sys
+# sys.stdin = open("rgb7500.in",'r')
+# sys.stdout = open("rgb7500.out",'w')
+s = []
+n = int(input())
+for i in range(n):
+    m = int(input())
+    s.append(m)
+j = sum(s)
+print(j)
